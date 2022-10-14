65165a3c335d4f0b8f1fb6bed4309a5bd2e66f7d
Max Rosoff <me@maxrosoff,com>
2022-10-13 21:34:41 -0700

    Small tweaks

--- src/components/pages/Schedule.tsx
+++ src/components/pages/Schedule.tsx
@@ -8,6 +8,21 @@ import { DateTime } from "luxon";
 import Socials from "../Socials";
 
 const Schedule = () => {
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [googleCalendarLink, setGoogleCalendarLink] = useState<string | undefined>();
+
+    const submitForm = (name: string, email: string, date: DateTime, reasonForContact: string) => {
+        setHasSubmitted(true);
+        const calendarEventTitle = "Consolutation With Julie Rosoff";
+        const details = `Hi there, my name is ${name} and I would like to schedule a consultation!%0AMy email is ${email}. Here are some details about what I would like to discuss:%0A%0A${reasonForContact}`;
+        const timeFormat = "yyyyMMdd'T'HHmmss";
+        const startTime = date.toFormat(timeFormat);
+        const endTime = date.plus({ hours: 2 }).toFormat(timeFormat);
+        setGoogleCalendarLink(
+            `https://calendar.google.com/calendar/u/0/r/eventedit?text=${calendarEventTitle}&dates=${startTime}/${endTime}&details=${details}&add=jdrosoff@gmail.com`
+        );
+    };
+
     return (
         <Box
             position={"absolute"}
@@ -25,30 +40,32 @@ const Schedule = () => {
                     Contact Me
                 </Typography>
                 <Socials />
-                <ContactForm />
+                {hasSubmitted ? (
+                    <ContactFormSubmitted googleCalendarLink={googleCalendarLink} />
+                ) : (
+                    <ContactForm submitForm={submitForm} />
+                )}
             </Box>
         </Box>
     );
 };
 
-const ContactForm = () => {
+const ContactForm = (props: { submitForm: Function }) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
-    const [reasonForContact, setReasonForContact] = useState("");
     const [date, setDate] = useState<DateTime | null>(null);
+    const [reasonForContact, setReasonForContact] = useState("");
 
     return (
-        <Box mt={8} sx={{ width: 800 }}>
-            <Box>
-                <TextField
-                    fullWidth
-                    required
-                    label={"Name"}
-                    value={name}
-                    onChange={(event) => setName(event.target.value)}
-                />
-            </Box>
+        <Box mt={8} width={800} display={"flex"} flexDirection={"column"}>
+            <TextField
+                fullWidth
+                required
+                label={"Name"}
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+            />
             <Box mt={3}>
                 <TextField
                     fullWidth
@@ -82,14 +99,43 @@ const ContactForm = () => {
                     label="Date And Time"
                     value={date}
                     onChange={(date) => setDate(date)}
-                    renderInput={(params) => <TextField fullWidth {...params} />}
+                    renderInput={(params) => <TextField fullWidth required {...params} />}
                 />
             </Box>
-            <Box mt={3}>
-                <Button variant={"outlined"} sx={{ p: 3, pt: 1, pb: 1 }}>
-                    Submit
+            <Box mt={3} alignSelf={"end"}>
+                <Button
+                    variant={"outlined"}
+                    size={"large"}
+                    sx={{ p: 5, pt: 1.2, pb: 1.2 }}
+                    disabled={!name || !email || !reasonForContact || !date}
+                    onClick={() => props.submitForm(name, email, date, reasonForContact)}
+                >
+                    Submit Message
                 </Button>
             </Box>
+        </Box>
+    );
+};
+
+const ContactFormSubmitted = (props: { googleCalendarLink: string | undefined }) => {
+    return (
+        <Box mt={8} display={"flex"} flexDirection={"column"}>
+            <Typography variant={"body1"} color={"white"}>
+                Thank you for contacting me! I will get back to you as soon as possible. Use the
+                links below to add this to your calendar.
+            </Typography>
+            {props.googleCalendarLink && (
+                <Button
+                    variant={"outlined"}
+                    size={"large"}
+                    sx={{ p: 5, pt: 1.2, pb: 1.2, mt: 4 }}
+                    href={props.googleCalendarLink}
+                    target={"_blank"}
+                    rel={"noopener"}
+                >
+                    Add To Google Calendar
+                </Button>
+            )}
         </Box>
     );
 };
